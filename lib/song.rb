183bbdbd0f2c82864859aa6a5a86571b20ed4741
GregT <taylor.gregory2@gmail.com>
2022-06-12 11:29:23 -0600

    commit tree 5a866720d543c075119e25059e1793303dc50efe

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+require "pry"
+
+class Song
+
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    @@genre_count = Hash.new
+    @@artist_count = Hash.new
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+
+end
